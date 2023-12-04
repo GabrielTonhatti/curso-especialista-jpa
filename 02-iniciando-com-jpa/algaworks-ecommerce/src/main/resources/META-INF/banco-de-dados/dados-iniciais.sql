c57c690240f9f3c0745e0db132acc0252f57f507
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-12-04 19:19:55 -0300

    2.3. Criando o persistence.xml

---
+++ 02-iniciando-com-jpa/algaworks-ecommerce/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUTO (ID, NOME, PRECO, DESCRICAO)
+VALUES (1, 'Kindle', 499.0,
+        'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');

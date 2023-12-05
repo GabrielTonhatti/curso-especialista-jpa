794f39f00ba34816e613f17f18130437410c6055
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-12-05 19:34:28 -0300

    2.9. Inserindo o primeiro objeto com o método persist

--- 02-iniciando-com-jpa/algaworks-ecommerce/src/test/java/com/algawors/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ 02-iniciando-com-jpa/algaworks-ecommerce/src/test/java/com/algawors/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -4,11 +4,33 @@ import com.algawors.ecommerce.EntityManagerTest;
 import com.algawors.ecommerce.model.Produto;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class OperacoesComTransacaoTest extends EntityManagerTest {
 
     @Test
-    void abrirEFecharATransacao() {
+    void inserirOPrimeiroObjeto() {
         Produto produto = new Produto();
+        produto.setId(2);
+        produto.setNome("Câmera Canon");
+        produto.setPreco(new BigDecimal(5000));
+        produto.setDescricao("A melhor definição para suas fotos");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        assertNotNull(produtoVerificacao);
+    }
+
+    @Test
+    void abrirEFecharATransacao() {
+//        Produto produto = new Produto(); // Somente para o método não mostrar erro
 
         entityManager.getTransaction().begin();
 

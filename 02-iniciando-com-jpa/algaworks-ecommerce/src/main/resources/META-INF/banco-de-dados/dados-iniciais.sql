59992b70972a5e877f35f6ff79a0b8c508b15e73
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-12-06 19:25:19 -0300

    2.15. Exercício: implementando um CRUD

--- 02-iniciando-com-jpa/algaworks-ecommerce/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ 02-iniciando-com-jpa/algaworks-ecommerce/src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,2 +1,5 @@
 INSERT INTO Produto (ID, NOME, PRECO, DESCRICAO) VALUES (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
 INSERT INTO Produto (ID, NOME, PRECO, DESCRICAO) VALUES (3, 'Câmera GoPro Hero 7', 1400.0, 'Desempenho 2x melhor.');
+
+INSERT INTO Cliente (ID, NOME) VALUES (1, 'Fernando Medeiros');
+INSERT INTO Cliente (ID, NOME) VALUES (2, 'Marcos Mariano');

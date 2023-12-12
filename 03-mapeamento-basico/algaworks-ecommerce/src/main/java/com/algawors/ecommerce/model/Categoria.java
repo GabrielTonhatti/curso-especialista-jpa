2e96cb9db8d8381abb12485fc99e1c2c0a9aa7b8
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2023-12-12 19:19:56 -0300

    3.7. Conhecendo as estratégias para geração de identificador com @GeneratedValue

--- 03-mapeamento-basico/algaworks-ecommerce/src/main/java/com/algawors/ecommerce/model/Categoria.java
+++ 03-mapeamento-basico/algaworks-ecommerce/src/main/java/com/algawors/ecommerce/model/Categoria.java
@@ -1,9 +1,6 @@
 package com.algawors.ecommerce.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +15,7 @@ public class Categoria {
     @Id
     @Column(name = "ID")
     @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column(name = "NOME")
---
+++ 03-mapeamento-basico/algaworks-ecommerce/src/test/java/com/algawors/ecommerce/mapeamentobasico/EstrategiaChavePrimariaTest.java
@@ -0,0 +1,28 @@
+package com.algawors.ecommerce.mapeamentobasico;
+
+import com.algawors.ecommerce.EntityManagerTest;
+import com.algawors.ecommerce.model.Categoria;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class EstrategiaChavePrimariaTest extends EntityManagerTest {
+
+    @Test
+    void testarEstrategiaAuto() {
+        Categoria categoria = new Categoria();
+        categoria.setNome("Eletrônicos");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoria);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+         assertNotNull(categoriaVerificacao);
+         assertEquals("Eletrônicos", categoriaVerificacao.getNome());
+    }
+
+}

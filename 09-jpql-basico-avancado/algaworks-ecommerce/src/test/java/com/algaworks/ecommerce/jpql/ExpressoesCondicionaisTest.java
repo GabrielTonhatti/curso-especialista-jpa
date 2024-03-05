c449f587ec91db9de037b949650c4df9a982fa12
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2024-03-05 19:11:30 -0300

    9.13. Usando expressões condicionais is null e is empty

--- 09-jpql-basico-avancado/algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ 09-jpql-basico-avancado/algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -1,7 +1,6 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Pedido;
 import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Test;
 
@@ -12,13 +11,33 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 class ExpressoesCondicionaisTest extends EntityManagerTest {
 
     @Test
+    void usarIsNull() {
+        String jpql = "SELECT p FROM Produto p WHERE p.foto IS NULL";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    void usarIsEmpty() {
+        String jpql = "SELECT p FROM Produto p WHERE p.categorias IS EMPTY";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+    }
+
+    @Test
     void usarExpressaoCondicionalLike() {
         String jpql = "SELECT c FROM Cliente c WHERE c.nome LIKE CONCAT('%', :nome, '%')";
 
-        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido .class);
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
         typedQuery.setParameter("nome", "a");
 
-        List<Pedido > lista = typedQuery.getResultList();
+        List<Object[]> lista = typedQuery.getResultList();
         assertFalse(lista.isEmpty());
     }
 }

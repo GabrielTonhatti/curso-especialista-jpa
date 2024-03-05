9764762a10126a26e4f41212a7711e7b8ec11974
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2024-03-05 19:52:37 -0300

    9.14. Usando expressões condicionais de maior e menor

--- 09-jpql-basico-avancado/algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ 09-jpql-basico-avancado/algaworks-ecommerce/src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -4,11 +4,24 @@ import com.algaworks.ecommerce.EntityManagerTest;
 import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class ExpressoesCondicionaisTest extends EntityManagerTest {
+
+    @Test
+    void usarMaiorMenor() {
+        String jpql = "SELECT p FROM Produto p WHERE p.preco >= :precoInicial AND p.preco <= :precoFinal";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        typedQuery.setParameter("precoInicial", new BigDecimal(400));
+        typedQuery.setParameter("precoFinal", new BigDecimal(1500));
+
+        List<Object[]> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+    }
 
     @Test
     void usarIsNull() {
